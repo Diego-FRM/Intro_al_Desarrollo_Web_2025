a44426226c27808edd72440a13570cd4219295f4
Diego-FRM <Dromer16@itam.mx>
2025-09-19 09:53:34 -0600

    4_funciones

---
+++ javascript/funciones.js
@@ -0,0 +1,12 @@
+// Función clásica para multiplicar dos números
+function multiplica_clasica(num1, num2) {
+    return num1 * num2;
+}
+// Funcion flecha para multiplicar dos números
+const multiplica_flecha = (num1, num2) => num1 * num2;
+// Función clasica para elevar al cuadrado
+function elevaAlCuadrado_clasica(num) {
+    return num * num;
+}
+// Función flecha para elevar al cuadrado
+const elevaAlCuadrado_flecha = num => num * num;
